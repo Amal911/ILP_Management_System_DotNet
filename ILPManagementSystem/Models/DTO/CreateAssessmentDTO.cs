dfd8d0179b01645f1c6d41d7e4f69274bc8a30c0
DHARSAN C SAJEEV <dharsancs999@gmail.com>
2024-07-30 11:32:40 +0530

    dev pull

--- ILPManagementSystem/Models/DTO/CreateAssessmentDTO.cs
+++ ILPManagementSystem/Models/DTO/CreateAssessmentDTO.cs
@@ -7,7 +7,7 @@ namespace ILPManagementSystem.Models.DTO
         public int BatchId { get; set; }
         public int PhaseId { get; set; }
         public string AssessmentTitle { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public int TotalScore { get; set; }
         public bool IsSubmitable { get; set; }
         public int UserId { get; set; }
