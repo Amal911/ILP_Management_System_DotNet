7f9320a338ac9afac9ab19f06ccde39a80d19d5a
amalea <amaleattath911@gmail.com>
2024-07-31 14:20:55 +0530

    big fix

--- ILPManagementSystem/Controllers/SessionController.cs
+++ ILPManagementSystem/Controllers/SessionController.cs
@@ -66,6 +66,35 @@ namespace ILPManagementSystem.Controllers
             }
         
         }
+        [HttpGet("{batchId}")]
+        public async Task<IActionResult> GetSessionsByBatchId(int batchId)
+        {
+
+            try
+            {
+                var sessions = await _sessionRepo.GetAllAsync();
+                var todaysSessions = sessions.Where(u=> u.BatchId == batchId).ToList();
+                var mappedSessions = _mapper.Map<IEnumerable<Session>>(todaysSessions);
+                var response = new APIResponse
+                {
+                    IsSuccess = true,
+                    Result = mappedSessions,
+                    StatusCode = HttpStatusCode.OK
+                };
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new APIResponse
+                {
+                    IsSuccess = false,
+                    Result = null,
+                    StatusCode = HttpStatusCode.InternalServerError,
+                });
+            }
+
+        }
 
         [HttpGet("{id:int}")]
         public async Task<ActionResult<APIResponse>> GetSession(int id)
